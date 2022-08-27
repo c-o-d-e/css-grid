1e498cfb6ca523bfc235d49aa30591783ec7e14c
c-o-d-e <swapyuk@gmail.com>
2022-08-27 01:17:32 +0100

    new divs with animation added

---
+++ index.js
@@ -0,0 +1,11 @@
+function enterFunc(event) {
+    event.target.style.display = "none";
+    event.target.parentElement.children[1].style.display = "flex";
+    event.target.parentElement.children[2].style.display = "flex";
+}
+
+function leaveFunc(event) {
+    event.target.children[1].style.display = "none";
+    event.target.lastElementChild.style.display = "none";
+    event.target.firstElementChild.style.display = "flex";
+}
